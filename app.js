79097cf3d6206ab3b76caa7ae33d73ca65d033cb
James Batho <j.batho325@gmail.com>
2021-05-19 03:32:00 -0400

    helpers and middleware complete

--- app.js
+++ app.js
@@ -7,8 +7,24 @@
  */
 
 const express = require("express");
+const cors = require("cors");
 
+const { NotFoundError } = require("./expressError");
+const { authenticateJWT } = require("./middleware/auth");
+
+const morgan = require("morgan");
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("tiny"));
+app.use(authenticateJWT);
+
+app.use("/auth", authRoutes);
+app.use("/users", usersRoutes);
+app.use("/pods", podsRoutes);
+app.use("/child", childRoutes);
+app.use("/appointments", appointmentsRoutes);
 
 /** Handle 404 errors -- this matches everything */
 app.use(function (req, res, next) {
